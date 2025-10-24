ab62fc8b51cc51e83e337bb7239a89b9890789bb
Pablo Otavio Dantas De Oliveira - FATEC - Cuiaba/MT <00587359@alunosenai.mt>
2025-10-23 21:17:26 -0400

    Criando tela de login

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,17 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+export default function Login(){
+    return(
+        <View>
+            <Text>
+                Só alegria!
+            </Text>
+        </View>
+    )
+}
+
+const style  = StyleSheet.create({
+    container:{
+        flex:1,
+    }
+});
